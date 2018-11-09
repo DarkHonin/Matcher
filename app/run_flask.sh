6a43bb10b7d7ddbafb428ee4e74d35c7be9c11f7
William GOURLEY <wgourley@e4r6p4.wethinkcode.co.za>
2018-11-09 11:07:23 +0200

    setup

---
+++ app/run_flask.sh
@@ -0,0 +1,7 @@
+####### Flask Config ########
+source bin/activate
+
+export FLASK_APP=src/index.py   #Set the entry point
+export FLASK_DEBUG=1            #Set for debuggin
+
+flask run
---
+++ app/step_py3_venv.sh
@@ -0,0 +1,16 @@
+echo "----------Installing brew--------------"
+if ! type "brew" > /dev/null; then
+    @sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tolsadus/42homebrewfix/master/install.sh)"
+fi
+echo "----------Installing Python 3--------------"
+if ! type "python3" > /dev/null; then
+    @brew install python3
+fi
+echo "----------Installing MongoDB--------------"
+if ! type "mongod" > /dev/null; then
+    @brew install mongodb
+fi
+echo "----------Installing python virtual env--------------"
+if ! type "virtualenv" > /dev/null; then
+    @pip3 install virtualenv
+fi
